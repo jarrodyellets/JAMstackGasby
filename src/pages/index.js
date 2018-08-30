4d1cb8be3ba74f7d9d2019e38b29b5fe9f736a19
jarrodyellets <jarrod524@gmail.com>
2018-08-30 08:52:03 +0200

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Good Day!</h1>
+    <p>My name is Jarrod Yellets</p>
+    <p>I am a web developer from Brussels, Belgium, and I will be attending the freeCodeCamp JAMstack hackathon.</p>
+    <a href="https://www.jarrodyellets.com" target="__blank">www.jarrodyellets.com</a>
+  </div>
+)
+
+export default IndexPage
